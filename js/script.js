bb562ce0e1f2855cb997e560004def8d7ce424cb
Sarthak Upadhyay <sarthak7u@gmail.com>
2016-01-25 21:56:15 +0530

    Improve button and field response

--- js/script.js
+++ js/script.js
@@ -5,16 +5,50 @@ function foo()
     document.getElementById('display').innerHTML = COUNT % 4;
     COUNT += 1;
 }
-
-document.getElementById('bpm').addEventListener('change', startCount);
+function sanityCheckVal(val)
+{
+    if (val <= 0)
+    {
+        alert("please enter a valid number");
+        return false;
+    }
+    return true;
+}
+function hideElem(elem_class)
+{
+    document.getElementsByClassName(elem_class)[0].style.display = 'none';
+}
+function showElem(elem_class)
+{
+    document.getElementsByClassName(elem_class)[0].style.display = 'inline-block';
+}
 
 function startCount(){
     stopCount();
     var bpm_value = document.getElementById('bpm').value;
-    // console.log(value);
+    if (!sanityCheckVal(bpm_value))
+    {
+        return;
+    }
     REFRESH = setInterval(foo, (60000 / bpm_value));
+    switchButton('start');
 }
 
 function stopCount(){
-   clearInterval(REFRESH);
+    clearInterval(REFRESH);
+    switchButton('stop');
 }
+
+function switchButton(btn){
+    if (btn == 'start')
+    {
+        hideElem(btn);
+        showElem('stop');
+    }
+    else
+    {
+        hideElem(btn);
+        showElem('start');
+    }
+}
+autoStartCount();
